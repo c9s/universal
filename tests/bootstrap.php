f3a6a363d7ea86a8978d0611b67bc0f8f7646d47
c9s <yoanlin93@gmail.com>
2017-06-02 14:09:33 +0800

    add phpunit5 polyfill

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,5 @@
 <?php
 require 'vendor/autoload.php';
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
