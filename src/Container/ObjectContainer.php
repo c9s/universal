dbd2a5f81568f52c9361fa4163267b253c39a252
c9s <yoanlin93@gmail.com>
2017-06-03 15:41:28 +0800

    Call create with the container itself as the first argument

--- src/Container/ObjectContainer.php
+++ src/Container/ObjectContainer.php
@@ -17,7 +17,7 @@ use Exception;
 
 class ObjectContainerException extends Exception {  }
 
-class ObjectContainer 
+class ObjectContainer
 {
     public $_builders = array();
 
@@ -43,43 +43,43 @@ class ObjectContainer
      * @param string $key
      * @param array $args
      */
-    public function getObject($key,$args = array())
+    public function getObject($key, array $args = array())
     {
-        if( isset( $this->_singletonObjects[ $key ] ) ) {
+        if (isset( $this->_singletonObjects[ $key ] ) ) {
             return $this->_singletonObjects[ $key ];
         }
-        elseif( $build = $this->getBuilder($key) ) {
-            if( isset($build['singleton']) && $build['singleton'] ) {
-                return $this->_singletonObjects[ $key ] = $this->_buildObject($build['builder'],$args);
+
+        if ($config = $this->getBuilder($key)) {
+
+            if (isset($config['singleton']) && $config['singleton']) {
+                return $this->_singletonObjects[$key] = $this->_buildObject($config['builder'], $args);
             }
-            return $this->_buildObject($build['builder'],$args);
-        }
-        else {
+
+            return $this->_buildObject($config['builder'], $args);
+
+        } else {
             if( $this->throwIfNotFound ) {
                 throw new ObjectContainerException("object builder not found: $key");
             }
         }
     }
 
-    protected function _buildObject($b,$args = array())
+    protected function _buildObject($b, array $args = array())
     {
-        if( is_callable($b) ) {
-            return call_user_func_array($b,$args);
-        } 
-        elseif( is_array($b) ) {
-            return call_user_func_array($b,$args);
-        }
-        elseif( is_string($b) ) {
+        if (is_callable($b)) {
+            return call_user_func_array($b, $args);
+        } else if (is_array($b)) {
+            return call_user_func_array($b, $args);
+        } else if (is_string($b)) {
             $callable = explode('#',$b);
             $class = $callable[0];
-            if( class_exists($class,true) ) {
+            if (class_exists($class,true) ) {
                 if( isset($callable[1]) ) {
                     return call_user_func_array($callable,$args);
                 } else {
                     return new $b;
                 }
-            }
-            else {
+            } else {
                 throw new ObjectContainerException("Can not build object from $b");
             }
         }
@@ -129,22 +129,22 @@ class ObjectContainer
     /**
      * Register a factory builder
      */
-    public function registerFactory($key,$builder)
+    public function factory($key, $builder)
     {
-        $this->_builders[ $key ] = array( 
-            'singleton' => false,
-            'builder' => $builder,
-        );
+        $this->_builders[$key] = [
+            "singleton" => false,
+            "builder" => $builder,
+        ];
     }
 
     public function __get($key)
     {
-        return $this->getObject($key);
+        return $this->getObject($key, [$this]);
     }
 
-    public function __set($key,$builder) 
+    public function __set($key, $builder)
     {
-        $this->setBuilder($key,$builder);
+        $this->setBuilder($key, $builder);
     }
 
     public function __isset($key)
